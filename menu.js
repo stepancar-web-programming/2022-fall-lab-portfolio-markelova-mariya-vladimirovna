c48186ec536483cf72346fed722ce05e2089b6c8
Mariia Markelova <92391145+mmrkl@users.noreply.github.com>
2022-12-29 11:32:36 +0300

    Delete menu.js

--- menu.js
+++
@@ -1,8 +0,0 @@
-function myFunction() {
-    var x = document.getElementById("navigation");
-    if (x.className === "nav") {
-        x.className += " responsive";
-    } else {
-        x.className = "nav";
-    }
-}
